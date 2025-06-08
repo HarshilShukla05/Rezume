466502c929863513e46a3282278cf31e3e65b219
HarshilShukla05 <harshilshukla0502@gmail.com>
2025-06-08 17:38:47 +0530

    Integrated LLama 4 agenting for curating resume

--- backend/src/utils/openai.ts
+++ backend/src/utils/openai.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from "openai";
 
 const openai = new OpenAI({
-    apiKey: "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"
+    apiKey: process.env.GROQ_API_KEY ,
 });
 
 export default openai;
